a66601353fd7d6aa4b0bfb59fe5e78e2d2ce9cf1
anujkarki <anuj1@sisnp.com>
2024-06-06 10:30:33 +0545

    Initial commit

---
+++ MAUIPushNotification/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUIPushNotification
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
