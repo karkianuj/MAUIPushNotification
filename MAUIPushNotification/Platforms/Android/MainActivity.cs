9db63aa57f708fa88a87a755dd5dcd6e3cd68e85
anujkarki <anuj1@sisnp.com>
2024-07-18 15:59:11 +0545

    Fixes

--- MAUIPushNotification/Platforms/Android/MainActivity.cs
+++ MAUIPushNotification/Platforms/Android/MainActivity.cs
@@ -4,12 +4,17 @@ using Android.OS;
 
 namespace MAUIPushNotification
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+    }
+    [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
     [IntentFilter(new[] { Android.Content.Intent.ActionView },
               Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable },
               DataScheme = CALLBACK_SCHEME)]
-    public class MainActivity : MauiAppCompatActivity
+    public class WebAuthenticationCallbackActivity : Microsoft.Maui.Authentication.WebAuthenticatorCallbackActivity
     {
         const string CALLBACK_SCHEME = "mauipushnotification";
+
     }
 }
